9b2c13eb624a1f98f6cdea1f164b3418942bc9d0
Edmundo Fuentes <me@edmundofuentes.com>
2016-09-01 20:18:28 -0500

    Updating editable TDs

--- bundles/Angle/NickelTracker/CoreBundle/Resources/public/js/nickeltracker.js
+++ bundles/Angle/NickelTracker/CoreBundle/Resources/public/js/nickeltracker.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
-    $('.pencil-on-hover').each(function(){
+    $('.editable-td').each(function(){
         $(this).append('<i class="fa fa-pencil"></i>');
+    });
+
+    $('.editable-td').on('click', function() {
+        var $this = $(this);
+        var $input = $('<input>', {
+            value: $this.text(),
+            type: 'text',
+            blur: function() {
+                $this.text(this.value);
+            },
+            keyup: function(e) {
+                if (e.which === 13) $input.blur();
+            }
+        }).appendTo( $this.empty() ).focus();
     });
 });
