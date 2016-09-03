3e9b0931825065163f4217a315717dde901e9ecd
Edmundo Fuentes <me@edmundofuentes.com>
2016-09-03 18:28:17 -0500

    Dump the dashboard contents

--- bundles/Angle/NickelTracker/AppBundle/Controller/DashboardController.php
+++ bundles/Angle/NickelTracker/AppBundle/Controller/DashboardController.php
@@ -8,7 +8,12 @@ class DashboardController extends Controller
 {
     public function homeAction()
     {
+        /** @var \Angle\NickelTracker\CoreBundle\Service\NickelTrackerService $nt */
+        $nt = $this->get('angle.nickeltracker');
+        $dashboard = $nt->loadDashboard();
+
         return $this->render('AngleNickelTrackerAppBundle:Dashboard:home.html.twig', array(
+            'dashboard' => $dashboard,
         ));
     }
 }
