9667f15c8a7b1c637a5e125024e1f830f958675e
Edmundo Fuentes <me@edmundofuentes.com>
2016-09-03 20:17:04 -0500

    Implementing password update

--- bundles/Angle/NickelTracker/AppBundle/Controller/UserController.php
+++ bundles/Angle/NickelTracker/AppBundle/Controller/UserController.php
@@ -3,6 +3,12 @@
 namespace Angle\NickelTracker\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Angle\NickelTracker\CoreBundle\Utility\ResponseMessage;
+use Angle\NickelTracker\CoreBundle\Entity\User;
 
 class UserController extends Controller
 {
@@ -20,6 +26,36 @@ class UserController extends Controller
 
     public function changePasswordAction(Request $request)
     {
-        
+        // Process new account
+        $oldPassword        = $request->request->get('oldPassword');
+        $newPassword        = $request->request->get('newPassword');
+        $confirmPassword    = $request->request->get('confirmPassword');
+
+        // Check the request parameters
+        if ($oldPassword && $newPassword && $confirmPassword && ($newPassword == $confirmPassword)) {
+            // Attempt to change the user password
+            /** @var \Angle\NickelTracker\CoreBundle\Service\NickelTrackerService $nt */
+            $nt = $this->get('angle.nickeltracker');
+            $r = $nt->changeUserPassword($oldPassword, $newPassword);
+
+            if ($r) {
+                // Everything went ok, redirect to the account list with a FlashBag
+                $message = new ResponseMessage(ResponseMessage::CUSTOM, 0);
+                $message->addToFlashBag($this->get('session')->getFlashBag());
+            } else {
+                $error = $nt->getError();
+                // Something failed, build a new Response Message and return to the create new view
+                $message = new ResponseMessage(ResponseMessage::CUSTOM, 1);
+                $message->setExternalMessage($error['code'] . ': ' . $error['message']);
+                $message->addToFlashBag($this->get('session')->getFlashBag());
+            }
+        } else {
+            // Invalid request parameters
+            $message = new ResponseMessage(ResponseMessage::CUSTOM, 1);
+            $message->addToFlashBag($this->get('session')->getFlashBag());
+        }
+
+
+        return $this->redirectToRoute('angle_nt_app_user_profile');
     }
 }
